d3261b36898cf9fa648c4d8d6c8b270c3113cd53
Amonkai <110460985+Amonkai@users.noreply.github.com>
2024-01-30 01:08:40 +0100

    visual correction

--- Frontend/AmazonPB/src/pages/cartPage/cart.tsx
+++ Frontend/AmazonPB/src/pages/cartPage/cart.tsx
@@ -11,20 +11,46 @@ function CartPage() {
 
     return (
         <List
-            sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-            subheader={<ListSubheader>Settings</ListSubheader>}
+            sx={{
+                width: '100%',
+                maxWidth: 360,
+                bgcolor: 'background.paper',
+                margin: 'auto',
+                marginTop: '5rem',
+                borderRadius: '10px',
+                boxShadow: '0px 0px 10px rgba(0,0,0,0.1)'
+            }}
+            subheader={
+                <ListSubheader component="div" sx={{ bgcolor: 'primary.main', color: 'white', borderRadius: '10px 10px 0 0' }}>
+                    Your Cart
+                </ListSubheader>
+            }
         >
-            {cart.map((item) => <ListItem>
-                <ListItemText id="switch-list-label-wifi" primary={item.name}/>
-                <ListItemText id="switch-list-label-wifi" primary={`${item.value} PLN`}/>
-                <Button onClick={() => Remove(item)}>Remove</Button>
-            </ListItem>)}
+            {cart.map((item, index) => (
+                <ListItem key={index} sx={{ borderBottom: '1px solid rgba(0,0,0,0.1)' }}>
+                    <ListItemText primary={item.name} secondary={`${item.value} PLN`} />
+                    <Button
+                        onClick={() => Remove(item)}
+                        variant="outlined"
+                        color="error"
+                        sx={{ ml: 2 }}
+                    >
+                        Remove
+                    </Button>
+                </ListItem>
+            ))}
             <ListItem>
-                <ListItemText id="switch-list-label-wifi" primary={`Razem: ${cart.reduce((sum, el) => sum + el.value, 0)} PLN`}/>
-                <Button onClick={() => navigate('/buy')}>Buy</Button>
+                <ListItemText primary={`Total: ${cart.reduce((sum, el) => sum + el.value, 0)} PLN`} />
+                <Button
+                    onClick={() => navigate('/buy')}
+                    variant="contained"
+                    color="primary"
+                    sx={{ ml: 2 }}
+                >
+                    Buy
+                </Button>
             </ListItem>
         </List>
-
     );
 }
 
